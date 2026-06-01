c5c735611c7f59891cf132715f20f4b4c67e1748
Matheus Nakagaki Gouveia <matheusnakagaki@gmail.com>
2026-06-01 13:37:02 -0300

    Implementando CarroController

---
+++ src/controllers/CarroController.ts
@@ -0,0 +1,63 @@
+import { Request, Response } from "express";
+import { CarroService } from "../services/CarroService";
+
+const carroService = new CarroService();
+
+export function cadastrarCarro(req: Request, res: Response) {
+  try {
+    const novoCarro = carroService.cadastrarCarro(req.body);
+    res.status(201).json({
+      mensagem: "Carro cadastrado com sucesso!",
+      cliente: novoCarro,
+    });
+  } catch (error: any) {
+    const mensagem = error.message;
+
+    if (mensagem === "Informações obrigatórias incompletas") {
+      return res.status(400).json({ mensagem: error.message });
+    }
+
+    if (mensagem.includes("Já existe")) {
+      return res.status(409).json({ mensagem: error.message });
+    }
+  }
+}
+
+export function listarCarros(req: Request, res: Response) {
+  const carros = carroService.listarCarros();
+  return res.status(200).json(carros);
+}
+
+export function buscarPorId(req: Request, res: Response) {
+  try {
+    const id = parseInt(req.params.id as string);
+    const carro = carroService.buscarPorId(id);
+    return res.status(200).json(carro);
+  } catch (error: any) {
+    return res.status(404).json({ mensagem: error.message });
+  }
+}
+
+export function atualizarCliente(req: Request, res: Response) {
+  try {
+    const id = parseInt(req.params.id as string);
+    const carroAtualizado = carroService.atualizarCarro(id, req.body);
+    return res.status(200).json(carroAtualizado);
+  } catch (error: any) {
+    if (error.message === "Carro não encontrado") return res.status(404).json({ mensagem: error.message });
+    return res.status(400).json({ mensagem: error.message });
+  }
+}
+
+export function removerCarro(req: Request, res: Response) {
+  try {
+    const id = parseInt(req.params.id as string);
+    carroService.removerCarro(id);
+    return res.status(200).json({ mensagem: "Carro removido com sucesso!" });
+  } catch (error: any) {
+    if (error.message.includes("Não é permitido remover")) return res.status(422).json({ mensagem: error.message });
+    if (error.message === "Carro não encontrado") return res.status(404).json({ mensagem: error.message });
+    return res.status(500).json({ mensagem: "Erro interno no servidor" });
+  }
+}
+
