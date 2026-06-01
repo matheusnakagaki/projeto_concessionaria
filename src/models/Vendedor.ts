f4ab809e78b3634c7529822c823fd344cfc86a1b
Matheus Nakagaki Gouveia <matheusnakagaki@gmail.com>
2026-05-31 23:13:04 -0300

    Criação da classe Vendedor

---
+++ src/models/Vendedor.ts
@@ -0,0 +1,14 @@
+export class Vendedor {
+    id_vendedor: number;
+    nome: string;
+    matricula: string;
+    comissao_percentual: number
+
+    constructor(id_vendedor: number, nome: string, matricula: string, comissao_percentual: number) {
+        this.id_vendedor = id_vendedor;
+        this.nome = nome;
+        this.matricula = matricula;
+        this.comissao_percentual = comissao_percentual
+    }
+}
+
