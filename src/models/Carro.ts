22ddca8166a4c33af2afe27f9ae5f7b5ad27cae4
Matheus Nakagaki Gouveia <matheusnakagaki@gmail.com>
2026-05-31 23:18:21 -0300

    Criando a classe Carro

---
+++ src/models/Carro.ts
@@ -0,0 +1,20 @@
+export class Carro {
+    id_carro: number;
+    marca: string;
+    modelo: string;
+    ano: number;
+    placa: string;
+    preco: number;
+    cor: string
+
+    constructor(id_carro: number, marca: string, modelo: string, ano: number, placa: string, preco: number, cor: string) {
+        this.id_carro = id_carro;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+        this.placa = placa;
+        this.preco = preco;
+        this.cor = cor
+    }
+}
+
